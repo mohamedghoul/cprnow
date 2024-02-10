c1b49d85d0139e7931f80fef78bf6ef651445330
Mohamed Ghoul <ghoul_mohamed@outlook.com>
2024-02-10 08:41:47 +0300

    Add accessibility features

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+window.addEventListener('load', function() {
+  new Accessibility();
+}, false);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
